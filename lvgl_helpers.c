1c77643a23367e44d343a89fe40a59b345f84a24
Sonder <332518853@qq.com>
2022-09-07 10:22:55 +0800

    modify

--- lvgl_helpers.c
+++ lvgl_helpers.c
@@ -154,12 +154,25 @@ bool lvgl_spi_driver_init(int host,
     int dma_channel,
     int quadwp_pin, int quadhd_pin)
 {
-    assert((0 <= host) && (SPI_HOST_MAX > host));
-    const char *spi_names[] = {
-        "SPI1_HOST", "SPI2_HOST", "SPI3_HOST"
-    };
 
-    ESP_LOGI(TAG, "Configuring SPI host %s", spi_names[host]);
+#if defined (CONFIG_IDF_TARGET_ESP32)
+    assert((SPI_HOST <= host) && (VSPI_HOST >= host));
+    const char *spi_names[] = {
+        "SPI_HOST", "HSPI_HOST", "VSPI_HOST"
+    };
+#elif defined (CONFIG_IDF_TARGET_ESP32S2)
+    assert((SPI_HOST <= host) && (HSPI_HOST >= host));
+    const char *spi_names[] = {
+        "SPI_HOST", "", ""
+    };
+#elif defined (CONFIG_IDF_TARGET_ESP32C3)
+    //assert((SPI_HOST <= host) && (HSPI_HOST >= host));
+    const char *spi_names[] = {
+        "SPI2_HOST", "", ""
+    };
+#endif
+
+    ESP_LOGI(TAG, "Configuring SPI host %s (%d)", spi_names[host], host);
     ESP_LOGI(TAG, "MISO pin: %d, MOSI pin: %d, SCLK pin: %d, IO2/WP pin: %d, IO3/HD pin: %d",
         miso_pin, mosi_pin, sclk_pin, quadwp_pin, quadhd_pin);
 
@@ -175,11 +188,11 @@ bool lvgl_spi_driver_init(int host,
     };
 
     ESP_LOGI(TAG, "Initializing SPI bus...");
-    #if defined (CONFIG_IDF_TARGET_ESP32C3)
+#if defined (CONFIG_IDF_TARGET_ESP32C3)
     dma_channel = SPI_DMA_CH_AUTO;
-    #endif
+#endif
     esp_err_t ret = spi_bus_initialize(host, &buscfg, (spi_dma_chan_t)dma_channel);
     assert(ret == ESP_OK);
-
+    
     return ESP_OK != ret;
 }
